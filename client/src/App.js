f33a94502ee6f190b61fc4e62ab0b311af4f5934
alno30 <alno30@comhem.com>
2018-11-08 15:00:55 +0100

    Simple Login landing page + single job component. No visual design

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.png';
+import React, {Component, Fragment} from 'react';
 import './App.css';
-import LoginModal from "./LoginModal.js";
+import StartPage from "./StartPage.js"
+import LoggedInLandingPage from "./LoggedInLandingPage";
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 
 class App extends Component {
-
-  state = {
-    showModal: false
-};
-
-showTheModal = () => {
-    this.setState({showModal:true})
-};
-
-hideModal = () => {
-    this.setState({showModal:false})
-
-};
 
 
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-        <img src={logo} alt=""/>
-        <h1>Apply for a new job today</h1>
-          <p onClick={this.showTheModal} className="enticingText">
-            Login
-          </p>
-      {   this.state.showModal && <LoginModal className="loginmodal" isOpen={this.state.showModal} onClose={this.hideModal}/>}
-        </header>
-      </div>
+      <Router>
+          <Fragment>
+          <Route exact path="/" component={StartPage}/>
+          <Route exact path="/welcome" component={LoggedInLandingPage}/>
+          </Fragment>
+      </Router>
     );
   }
 }
