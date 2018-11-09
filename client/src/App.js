709ef2097282ce068fe701d931f2ae0ac68b14f2
alno30 <alno30@comhem.com>
2018-11-09 13:24:10 +0100

    Added various functions related to the login and signup function, including saving of username and login token as well as a post-method for signup. Note: the signup functionality should probably not be tested due to backend code not being done yet

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component, Fragment} from 'react';
 import './App.css';
 import StartPage from "./StartPage.js"
+import SignUpPage from "./SignUpPage";
 import LoggedInLandingPage from "./LoggedInLandingPage";
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 
@@ -14,6 +15,7 @@ class App extends Component {
           <Fragment>
           <Route exact path="/" component={StartPage}/>
           <Route exact path="/welcome" component={LoggedInLandingPage}/>
+              <Route exact path="/signup" component={SignUpPage}/>
           </Fragment>
       </Router>
     );
